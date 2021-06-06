c5497c19294929d5579e6abf6aa8adc71598501b
Oak Arboleda <oakarboleda@gmail.com>
2021-06-06 13:26:36 -0500

    First commit

---
+++ install.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+/usr/sbin/softwareupdate --install-rosetta --agree-to-license
+
+
+#Check if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo 'Please install Homebrew by running the following command: arm /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+"'
+else
+    brew update
+fi
+
+#Upgrade installed formulae
+brew upgrade
+
+# Save Homebrew’s installed location
+BREW_PREFIX=$(brew --prefix)
+
+xcode-select --install
+# MySQL install
+arm brew install mysql
+arm brew services start mysql
+
+# Composer 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
+composer global require laravel/valet
+valet install
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+#Tools
+brew tap mongodb/brew
+brew install mongodb-community
+
+brew install wget --with-iri
+brew install gmp
+brew install grep
+brew install node
+
+brew install git
+brew install git-lfs
+brew install github/gh/gh
+
+brew install zsh
+
+#Apps
+brew install --cask dropbox
+brew install --cask firefox
+brew install --cask homebrew/cask-versions/firefox-nightly
+brew install --cask google-chrome
+brew install --cask homebrew/cask-versions/google-chrome-canary
+brew install --cask iterm2
+brew install --cask notion
+brew install --cask spotify
+brew install --cask vlc
+brew install --cask tower
+brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask karabiner-elements
+brew install --cask discord
+brew install --cask slack
+
+# Remove outdated versions from the cellar.
+brew cleanup
