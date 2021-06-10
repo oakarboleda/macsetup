c671741d723b8267028359adbf537ac3a3445707
Oak Arboleda <oakarboleda@gmail.com>
2021-06-10 12:42:45 -0500

    updates

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
-    echo 'Please install Homebrew by running the following command: arm /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    echo 'Please install Homebrew by running the following command: arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")"
 "'
 else
     brew update
@@ -18,7 +18,9 @@ brew upgrade
 # Save Homebrew’s installed location
 BREW_PREFIX=$(brew --prefix)
 
+#Xcode install
 xcode-select --install
+
 # MySQL install
 arm brew install mysql
 arm brew services start mysql
@@ -38,20 +40,19 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+nvm install node 12
 
 #Tools
-brew tap mongodb/brew
-brew install mongodb-community
-
-brew install wget --with-iri
+brew install wget
 brew install gmp
 brew install grep
-brew install node
+
 
 brew install git
 brew install git-lfs
 brew install github/gh/gh
 
+#ZSH install
 brew install zsh
 
 #Apps
@@ -61,14 +62,8 @@ brew install --cask homebrew/cask-versions/firefox-nightly
 brew install --cask google-chrome
 brew install --cask homebrew/cask-versions/google-chrome-canary
 brew install --cask iterm2
-brew install --cask notion
-brew install --cask spotify
-brew install --cask vlc
-brew install --cask tower
 brew install --cask visual-studio-code
 brew install --cask zoom
-brew install --cask karabiner-elements
-brew install --cask discord
 brew install --cask slack
 
 # Remove outdated versions from the cellar.
